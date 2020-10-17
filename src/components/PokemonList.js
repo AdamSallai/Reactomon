6dba8c0615487923c273572b1f75a797a1ac874e
Adam Sallai <yadam1998@gmail.com>
2020-10-17 22:46:54 +0200

    working infinite scroll but it rerenders too many times

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,30 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import axios from "axios";
 import Pokemon from "./Pokemon";
 import styled from "styled-components";
 
-
 export const PokemonList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [pokemons, setPokemons] = useState([]);
-
+  const [pageNumber, setPageNumber] = useState(1);
+  const [nextUrl, setNextUrl] = useState("https://pokeapi.co/api/v2/pokemon");
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-      setPokemons(res.data.results);
-      setIsLoading(false);
-    });
-  },[])
+    setIsLoading(true);
+    let cancel;
+    axios({
+      method: "GET",
+      url: nextUrl,
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setPokemons((prevPokemons) => [...prevPokemons, ...res.data.results]);
+        setNextUrl(res.data.next);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+    return () => cancel();
+  }, [pageNumber]);
 
-  if (isLoading) {
-    return <p></p>;
-  } else {
-    const allPokemons = pokemons.map((pokemon) => (
-      <Pokemon key={pokemon.name} pokemon={pokemon} />
-    ));
-    return <Pokemons className="pokemons">{allPokemons}</Pokemons>;
-  }
-}
+  const observer = useRef();
+  const lastPokemonRef = useCallback(
+    (node) => {
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isLoading]
+  );
+  const allPokemons = pokemons.map((pokemon, index) => {
+    if (pokemons.length === index + 1) {
+      console.log(pokemon.name);
+      return (
+        <div key={pokemon.name} ref={lastPokemonRef}>
+          <Pokemon pokemon={pokemon} />
+        </div>
+      );
+    } else {
+      return (
+        <div key={pokemon.name}>
+          <Pokemon pokemon={pokemon} />
+        </div>
+      );
+    }
+  });
+  return <Pokemons className="pokemons">{allPokemons}</Pokemons>;
+};
 
 const Pokemons = styled.div`
   display: flex;
@@ -32,7 +67,6 @@ const Pokemons = styled.div`
   align-items: center;
   flex-wrap: wrap;
   position: relative;
-`
-
+`;
 
 export default PokemonList;
